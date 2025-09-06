6b9fe4118c949b6970ef20425458e576d8870c24
cla6shade <cla6shade@gmail.com>
2025-09-06 22:27:39 +0900

    design: 네비게이션 트리거를 헤더 컴포넌트로 대체

---
+++ src/features/navigation/PageHeader.tsx
@@ -0,0 +1,34 @@
+import brand from "@/brand/baseInfo";
+import colors from "@/brand/colors";
+import { Box, Button, Text } from "@chakra-ui/react";
+
+interface PageHeaderProps {
+  onOpen: () => void;
+}
+
+export default function PageHeader({ onOpen }: PageHeaderProps) {
+  return (
+    <Box
+      position="fixed"
+      w="full"
+      h="48px"
+      bg={colors.brandPanel}
+      top={0}
+      left={0}
+      display={{ base: "flex", lg: "none" }}
+      alignItems="center"
+    >
+      <Button
+        onClick={onOpen}
+        zIndex={1000}
+        variant="solid"
+        color={colors.brand}
+        bg={colors.brandPanel}
+        size="sm"
+      >
+        ☰
+      </Button>
+      <Text color="white">{brand.name}</Text>
+    </Box>
+  );
+}
