c31d76ffc1611be4f90906f04f72374dfd3bfaf4
cla6shade <cla6shade@gmail.com>
2025-09-06 19:03:50 +0900

    feat: 메뉴 선택 훅 구현

---
+++ src/features/navigation/menu/menus.ts
@@ -0,0 +1,40 @@
+export const adminMenus = [
+  {
+    name: "레슨 시간표",
+    href: "/teacher",
+  },
+  {
+    name: "수강생 관리",
+    href: "/teacher/student-management",
+  },
+  {
+    name: "선생님 관리",
+    href: "/teacher/teacher-management",
+  },
+  {
+    name: "결제 정보 추가",
+    href: "/teacher/payment-management",
+  },
+];
+
+export const userMenus = [
+  {
+    name: "레슨 시간표",
+    href: "/user",
+  },
+  {
+    name: "내 정보",
+    href: "/user/my-info",
+  },
+  {
+    name: "레슨 내역",
+    href: "/user/lesson-history",
+  },
+  {
+    name: "결제 내역",
+    href: "/user/payment-history",
+  },
+];
+
+export const DEFAULT_ADMIN_MENU = adminMenus[0];
+export const DEFAULT_USER_MENU = userMenus[0];
---
+++ src/features/navigation/menu/useMenu.tsx
@@ -0,0 +1,15 @@
+import { usePathname } from "next/navigation";
+import { adminMenus, userMenus } from "./menus";
+import { useNavigation } from "../location/NavigationContext";
+
+export default function useMenu() {
+  const { isAdmin } = useNavigation();
+  const currentPath = usePathname();
+  const currentMenu =
+    adminMenus.find((menu) => menu.href === currentPath) ||
+    userMenus.find((menu) => menu.href === currentPath);
+  return {
+    menus: isAdmin ? adminMenus : userMenus,
+    currentMenu,
+  };
+}
