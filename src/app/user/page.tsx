9af57e51b70386f1a736a8be362dfdcf73abb5be
cla6shade <cla6shade@gmail.com>
2025-09-08 09:29:13 +0900

    refactor: suspense data query

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -1,28 +1,35 @@
 import ContentLayout from "@/components/ContentLayout";
+import { CenteredSpinner } from "@/components/Spinner";
 import LessonTablePage from "@/features/table/LessonTablePage";
 import prisma from "@/lib/prisma";
 import { getSession } from "@/lib/session";
+import { Suspense } from "react";
 
 export default async function UserDefaultPage() {
   const { locationId } = await getSession();
-  const workingTimes = await prisma.workingTime.findMany();
-  const openHours = await prisma.openHours.findFirst();
-  const teachers = await prisma.teacher.findMany({
+
+  const workingTimes = prisma.workingTime.findMany();
+  const openHours = prisma.openHours.findFirst();
+  const teachers = prisma.teacher.findMany({
     where: {
       locationId,
       isLeaved: false,
       isManager: false,
     },
+    include: {
+      workingTime: true,
+      major: true,
+    },
   });
-
-  const { startHour, endHour } = openHours!;
   return (
     <ContentLayout>
-      <LessonTablePage
-        workingTimes={workingTimes}
-        openHours={openHours!}
-        teachers={teachers}
-      />
+      <Suspense fallback={<CenteredSpinner />}>
+        <LessonTablePage
+          workingTimesPromise={workingTimes}
+          openHoursPromise={openHours}
+          teachersPromise={teachers}
+        />
+      </Suspense>
     </ContentLayout>
   );
 }
