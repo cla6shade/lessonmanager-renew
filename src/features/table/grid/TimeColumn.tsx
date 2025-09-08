82b5dfea4d011715a63e7fb16970ac5eb7d80ec7
cla6shade <cla6shade@gmail.com>
2025-09-08 09:36:03 +0900

    feat: 오픈 시간 컬럼 정의

---
+++ src/features/table/grid/TimeColumn.tsx
@@ -0,0 +1,28 @@
+import { getTimesInPeriod } from "@/utils/date";
+import { useTable } from "../TableProvider";
+import { Flex, Grid, Text } from "@chakra-ui/react";
+
+export default function TimeColumn() {
+  const { openHours } = useTable();
+  const times = getTimesInPeriod(openHours);
+  return (
+    <Grid
+      templateRows={`0.5fr repeat(${times.length}, 1fr) 0.5fr`}
+      width="88px"
+      flexShrink={0}
+    >
+      <div />
+      {times.map((time) => (
+        <Flex
+          key={`time-key-${time}`}
+          alignItems="center"
+          justifyContent="center"
+          width="full"
+          height="full"
+        >
+          <Text>{time}:00</Text>
+        </Flex>
+      ))}
+    </Grid>
+  );
+}
