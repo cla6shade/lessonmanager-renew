b334c91757450186e4b57fe93dd806c6f340b948
cla6shade <cla6shade@github.com>
2025-09-08 16:58:15 +0900

    chore: setup vitest

---
+++ scripts/test/vitest.setup.ts
@@ -0,0 +1,15 @@
+import { vi } from "vitest";
+import { createPrismock } from "prismock";
+import { PrismaClient } from "@/generated/prisma";
+
+vi.mock("@/generated/prisma", async () => {
+  const actual = await vi.importActual("@/generated/prisma");
+  return {
+    ...actual,
+    PrismaClient: createPrismock(actual.Prisma as any),
+  };
+});
+
+vi.mock("@/lib/prisma", async () => {
+  return { default: new PrismaClient() };
+});
