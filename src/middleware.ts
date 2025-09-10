42578780626b4b486d6d8417866c8c428a9d11e7
cla6shade <cla6shade@github.com>
2025-09-10 21:01:04 +0900

    chore: add error handling into the middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,29 +5,56 @@ import {
   DEFAULT_ADMIN_MENU,
   DEFAULT_USER_MENU,
 } from "./features/navigation/menu/menus";
+import z from "zod";
 
 export async function middleware(request: NextRequest) {
-  const session = await getSession();
-  if (!session.isLoggedIn) {
-    if (request.nextUrl.pathname === "/login") {
-      return NextResponse.next();
+  try {
+    const session = await getSession();
+    if (!session.isLoggedIn) {
+      if (request.nextUrl.pathname === "/login") {
+        return NextResponse.next();
+      }
+      if (request.nextUrl.pathname.startsWith("/api")) {
+        return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+      }
+      return NextResponse.redirect(new URL("/login", request.url));
     }
-    if (request.nextUrl.pathname.startsWith("/api")) {
-      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
-    }
-    return NextResponse.redirect(new URL("/login", request.url));
-  }
 
-  if (request.nextUrl.pathname === "/login") {
-    return NextResponse.redirect(
-      new URL(
-        session.isAdmin ? DEFAULT_ADMIN_MENU.href : DEFAULT_USER_MENU.href,
-        request.url
-      )
+    if (request.nextUrl.pathname === "/login") {
+      return NextResponse.redirect(
+        new URL(
+          session.isAdmin ? DEFAULT_ADMIN_MENU.href : DEFAULT_USER_MENU.href,
+          request.url
+        )
+      );
+    }
+
+    if (!session.isAdmin) {
+      if (request.nextUrl.pathname.startsWith("/api/admin")) {
+        return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+      }
+
+      if (request.nextUrl.pathname.startsWith("/teacher")) {
+        return NextResponse.redirect(new URL("/user", request.url));
+      }
+    }
+
+    return NextResponse.next();
+  } catch (error) {
+    console.error("Error in middleware:", error);
+    if (error instanceof z.ZodError) {
+      return NextResponse.json(
+        {
+          errors: z.flattenError(error),
+        },
+        { status: 400 }
+      );
+    }
+    return NextResponse.json(
+      { error: "Internal server error" },
+      { status: 500 }
     );
   }
-
-  return NextResponse.next();
 }
 
 export const config = {
