f38a513df6006842e45b0d009157c6f21e082b10
cla6shade <cla6shade@github.com>
2025-09-10 23:12:31 +0900

    feat: 사용자 검색 기능 구현 (관리자 기능)

---
+++ src/app/api/admin/users/route.ts
@@ -0,0 +1,80 @@
+import prisma from "@/lib/prisma";
+import { NextRequest, NextResponse } from "next/server";
+import { getPaginationQuery } from "../../utils";
+import { UserSearchRequestSchema, UserSearchResponse } from "./schema";
+import { Prisma } from "@/generated/prisma";
+
+export default async function GET(request: NextRequest) {
+  const { searchParams } = new URL(request.url);
+  const queryParams = {
+    name: searchParams.get("name") || undefined,
+    contact: searchParams.get("contact") || undefined,
+    locationId: searchParams.get("locationId") || undefined,
+    page: searchParams.get("page") || "1",
+    limit: searchParams.get("limit") || "20",
+  };
+
+  const validatedParams = UserSearchRequestSchema.parse(queryParams);
+  const { name, contact, locationId, page, limit } = validatedParams;
+  const whereConditions = getWhereConditions(name, contact, locationId);
+
+  const [users, total] = await Promise.all([
+    prisma.user.findMany({
+      where: whereConditions,
+      include: {
+        location: true,
+        teacherInCharge: {
+          omit: {
+            password: true,
+          },
+        },
+        latestLesson: true,
+      },
+      omit: {
+        password: true,
+      },
+      orderBy: {
+        registeredAt: "desc",
+      },
+      ...getPaginationQuery(page, limit),
+    }),
+    prisma.user.count({
+      where: whereConditions,
+    }),
+  ]);
+
+  const totalPages = Math.ceil(total / limit);
+
+  return NextResponse.json<UserSearchResponse>({
+    data: users,
+    total,
+    totalPages,
+  });
+}
+
+function getWhereConditions(
+  name?: string,
+  contact?: string,
+  locationId?: number
+) {
+  const whereConditions: Prisma.UserWhereInput = {
+    isLeaved: false,
+  };
+
+  if (name !== undefined) {
+    whereConditions.name = {
+      contains: name,
+    };
+  }
+
+  if (contact !== undefined) {
+    whereConditions.contact = {
+      contains: contact,
+    };
+  }
+
+  if (locationId !== undefined) {
+    whereConditions.locationId = locationId;
+  }
+  return whereConditions;
+}
---
+++ src/app/api/admin/users/schema.ts
@@ -0,0 +1,17 @@
+import { PublicUserSchema, PaginatedDataResponseSchema } from "../../schema";
+import { z } from "zod";
+export const UserSearchRequestSchema = z.object({
+  name: z.string().optional(),
+  contact: z.string().optional(),
+  locationId: z.coerce.number().optional(),
+  page: z.coerce.number().min(1).default(1),
+  limit: z.coerce.number().min(1).max(100).default(20),
+});
+
+export type UserSearchRequest = z.infer<typeof UserSearchRequestSchema>;
+
+export const UserSearchResponseSchema = PaginatedDataResponseSchema(
+  z.array(PublicUserSchema)
+);
+
+export type UserSearchResponse = z.infer<typeof UserSearchResponseSchema>;
