874f8cdb9828b27801ae35696561fd8a0e2aabd0
cla6shade <cla6shade@github.com>
2025-09-10 23:15:07 +0900

    feat: 본인 정보 조회 스키마 작성

---
+++ src/app/api/users/schema.ts
@@ -0,0 +1,6 @@
+import { z } from "zod";
+import { DataResponseSchema, PublicUserSchema } from "../schema";
+
+export const SingleUserResponseSchema = DataResponseSchema(PublicUserSchema);
+
+export type SingleUserResponse = z.infer<typeof SingleUserResponseSchema>;
