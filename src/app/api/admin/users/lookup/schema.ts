8e1227c92694fddd48f46ee827c68733cce16daf
cla6shade <cla6shade@github.com>
2025-09-10 23:14:20 +0900

    chore: 사용자 간단 검색 스키마 작성

---
+++ src/app/api/admin/users/lookup/schema.ts
@@ -0,0 +1,18 @@
+import { DataResponseSchema, PublicUserSchema } from "@/app/api/schema";
+import z from "zod";
+
+export const UserLookupRequestSchema = z.object({
+  query: z.string(),
+});
+
+export type UserLookupRequest = z.infer<typeof UserLookupRequestSchema>;
+
+export const UserLookupResponseSchema = DataResponseSchema(
+  PublicUserSchema.pick({
+    id: true,
+    name: true,
+    contact: true,
+  }).array()
+);
+
+export type UserLookupResponse = z.infer<typeof UserLookupResponseSchema>;
