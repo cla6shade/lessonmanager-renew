f3e43e9487d502bb979a89d2c99db75348eaf7c5
cla6shade <cla6shade@github.com>
2025-09-11 22:15:02 +0900

    fix: 불필요한 정보 제거

--- src/app/(lessons)/api/lessons/route.ts
+++ src/app/(lessons)/api/lessons/route.ts
@@ -25,7 +25,6 @@ export async function GET(request: NextRequest) {
             id: true,
             name: true,
             major: true,
-            location: true,
           },
         },
       },
