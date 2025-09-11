34fba5d90910508bee5670d2306cdde3f43eed14
cla6shade <cla6shade@github.com>
2025-09-11 22:16:31 +0900

    feat: 레슨 조회/업데이트 타입 작성

---
+++ src/app/(lessons)/api/lessons/[id]/schema.ts
@@ -0,0 +1,27 @@
+import { DataResponseSchema } from "@/app/schema";
+import { LessonSchema, LocationSchema, TeacherSchema } from "@/generated/zod";
+import z from "zod";
+
+export const GetLessonDetailResponseSchema = DataResponseSchema(
+  LessonSchema.extend({
+    teacher: TeacherSchema.pick({
+      id: true,
+      name: true,
+      major: true,
+    }),
+    location: LocationSchema,
+  })
+);
+
+export type GetLessonDetailResponse = z.infer<
+  typeof GetLessonDetailResponseSchema
+>;
+
+export const UpdateLessonRequestSchema = LessonSchema.pick({
+  note: true,
+  isDone: true,
+});
+
+export type UpdateLessonRequest = z.infer<typeof UpdateLessonRequestSchema>;
+
+export type UpdateLessonResponse = GetLessonDetailResponse;
