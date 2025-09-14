c040e027e767954f8e2feeff2f51a9d615b1bc46
cla6shade <cla6shade@gmail.com>
2025-09-14 11:19:28 +0900

    refactor: 사용자 레슨 폼 컴포넌트 분리

---
+++ src/features/dialog/UserLessonsForm.tsx
@@ -0,0 +1,100 @@
+import { VStack, Table, Checkbox, Textarea } from "@chakra-ui/react";
+import { Controller, useForm } from "react-hook-form";
+
+import { useUpdateLessons } from "@/features/dialog/useUpdateLessons";
+import { formatDate, formatHour } from "@/utils/date";
+import { UpdateLessonsRequest } from "@/app/(lessons)/api/lessons/schema";
+import { UserLessonsResponse } from "@/app/(users)/api/users/[id]/lessons/schema";
+
+interface UserLessonsFormProps {
+  lessons: UserLessonsResponse["data"];
+}
+
+export default function UserLessonsForm({ lessons }: UserLessonsFormProps) {
+  const { updateLessons } = useUpdateLessons();
+
+  const defaultLessons: UpdateLessonsRequest["lessons"] = lessons.map(
+    (lesson) => ({
+      id: lesson.id,
+      isDone: lesson.isDone,
+      note: lesson.note ?? "",
+    })
+  );
+
+  const { control, handleSubmit, register } = useForm<UpdateLessonsRequest>({
+    defaultValues: { lessons: defaultLessons },
+  });
+
+  const onSubmit = async (data: UpdateLessonsRequest) => {
+    await updateLessons(data);
+  };
+
+  return (
+    <form id="lessons-form" onSubmit={handleSubmit(onSubmit)}>
+      <VStack gap={4} align="stretch">
+        <Table.Root variant="line" size="sm">
+          <Table.Header>
+            <Table.Row>
+              <Table.ColumnHeader>상태</Table.ColumnHeader>
+              <Table.ColumnHeader>날짜</Table.ColumnHeader>
+              <Table.ColumnHeader>시간</Table.ColumnHeader>
+              <Table.ColumnHeader>담당 선생님</Table.ColumnHeader>
+              <Table.ColumnHeader>위치</Table.ColumnHeader>
+              <Table.ColumnHeader>분류</Table.ColumnHeader>
+              <Table.ColumnHeader>메모</Table.ColumnHeader>
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            {lessons.map((lesson, index) => (
+              <Table.Row key={lesson.id}>
+                <Table.Cell>
+                  <Controller
+                    control={control}
+                    name={`lessons.${index}.isDone`}
+                    render={({ field: { onChange, value } }) => (
+                      <Checkbox.Root
+                        checked={value}
+                        onCheckedChange={(details) =>
+                          onChange(!!details.checked)
+                        }
+                      >
+                        <Checkbox.HiddenInput />
+                        <Checkbox.Control>
+                          <Checkbox.Indicator />
+                        </Checkbox.Control>
+                      </Checkbox.Root>
+                    )}
+                  />
+                  <input
+                    type="hidden"
+                    {...register(`lessons.${index}.id`)}
+                    value={lesson.id}
+                  />
+                </Table.Cell>
+                <Table.Cell>{formatDate(new Date(lesson.dueDate))}</Table.Cell>
+                <Table.Cell>{formatHour(lesson.dueHour)}</Table.Cell>
+                <Table.Cell>
+                  {lesson.teacher.major.symbol} {lesson.teacher.name}
+                </Table.Cell>
+                <Table.Cell>{lesson.location.name}</Table.Cell>
+                <Table.Cell>
+                  {lesson.isGrand ? "그랜드 레슨" : "일반 레슨"}
+                </Table.Cell>
+                <Table.Cell>
+                  <Textarea
+                    {...register(`lessons.${index}.note`)}
+                    placeholder="레슨 노트를 입력하세요"
+                    size="sm"
+                    minH="60px"
+                    maxW="200px"
+                    resize="vertical"
+                  />
+                </Table.Cell>
+              </Table.Row>
+            ))}
+          </Table.Body>
+        </Table.Root>
+      </VStack>
+    </form>
+  );
+}
