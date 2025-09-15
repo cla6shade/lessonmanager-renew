d8d8fb101acd85e79c226917d4cc626225d48af3
cla6shade <cla6shade@github.com>
2025-09-15 17:47:55 +0900

    feat: update hook for banned time modification

---
+++ src/features/table/dialog/ban/useUpdateBannedTimes.ts
@@ -0,0 +1,31 @@
+import { useCallback } from "react";
+import {
+  UpdateBannedTimesRequest,
+  UpdateBannedTimesResponse,
+} from "@/app/(table)/api/bans/schema";
+import { useUpdate, UpdateResult } from "@/hooks/useUpdate";
+
+export function useUpdateBannedTimes() {
+  const { update, isSaving, error } = useUpdate<
+    UpdateBannedTimesRequest,
+    UpdateBannedTimesResponse
+  >();
+
+  const updateBannedTimes = useCallback(
+    async (
+      data: UpdateBannedTimesRequest
+    ): Promise<UpdateResult<UpdateBannedTimesResponse>> => {
+      return await update(data, {
+        endpoint: "/api/bans",
+        method: "PUT",
+      });
+    },
+    [update]
+  );
+
+  return {
+    updateBannedTimes,
+    isSaving,
+    error,
+  };
+}
