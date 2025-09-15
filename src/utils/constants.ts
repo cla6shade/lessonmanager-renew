e61e81de2179ce18a7698abb12f820cd7ffbeebf
cla6shade <cla6shade@gmail.com>
2025-09-15 09:19:07 +0900

    chore: lesson history constants

---
+++ src/utils/constants.ts
@@ -0,0 +1,16 @@
+export enum LessonModifyType {
+  CANCEL_LESSON = 0,
+  CREATE_LESSON = 1,
+  UPDATE_LESSON = 2,
+}
+export enum LessonModifierType {
+  USER = 0,
+  TEACHER = 1,
+}
+
+export function getModifyTypeText(type: LessonModifyType) {
+  return ["취소", "예약"][type];
+}
+export function getModifierTypeText(type: LessonModifierType) {
+  return ["회원", "선생님"][type];
+}
