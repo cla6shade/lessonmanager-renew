bc23bff1c3347d9adb469fa32c0d16f7428e9ae4
cla6shade <cla6shade@github.com>
2025-09-19 22:34:08 +0900

    feat: 레슨 생성 내역 기록

--- src/app/(history)/service.ts
+++ src/app/(history)/service.ts
@@ -1,18 +1,25 @@
 import prisma from "@/lib/prisma";
 import { LessonModifierType, LessonModifyType } from "@/utils/constants";
 
-export async function createModifyHistory(
-  isAdmin: boolean,
-  userId: number | undefined,
-  teacherId: number,
+export async function createModifyHistory({
+  isAdmin,
+  userId,
+  teacherId,
+  lesson,
+  createdById,
+  type,
+}: {
+  isAdmin: boolean;
+  userId: number | undefined;
+  teacherId: number;
   lesson: {
     id: number;
     dueDate: Date;
     dueHour: number;
-  },
-  createdById: number,
-  type: LessonModifyType
-) {
+  };
+  createdById: number;
+  type: LessonModifyType;
+}) {
   return prisma.lessonModifyHistory.create({
     data: {
       userId,
