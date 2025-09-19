06a4347aa96dec7291ac966bce8a7f8d9de243e8
cla6shade <cla6shade@github.com>
2025-09-19 22:42:29 +0900

    chore: 개발 모드일 경우 prisma log 출력

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,7 @@
 import { PrismaClient } from "@/generated/prisma";
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  log: process.env.NODE_ENV === "development" ? ["query"] : [],
+});
 
 export default prisma;
