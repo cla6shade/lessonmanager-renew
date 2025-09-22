ef36f948334bd167e08824eda1e965d94d85b383
cla6shade <cla6shade@gmail.com>
2025-09-22 10:36:31 +0900

    feat: user management page

--- src/features/users/UserManagementPage.tsx
+++ src/features/users/UserManagementPage.tsx
@@ -1,8 +1,20 @@
 "use client";
 
-import { useNavigation } from "../navigation/location/NavigationContext";
+import { Flex } from "@chakra-ui/react";
+import { FilterProvider } from "./FilterProvider";
+import { UserTableProvider } from "./UserTableProvider";
+import FilterSection from "./FilterSection";
+import UserTable from "./UserTable";
 
 export default function UserManagementPage() {
-  const { selectedLocation } = useNavigation();
-  return <div>{selectedLocation.name}</div>;
+  return (
+    <FilterProvider>
+      <UserTableProvider>
+        <Flex py={4} flexGrow={1} direction="column" height="100%">
+          <FilterSection />
+          <UserTable />
+        </Flex>
+      </UserTableProvider>
+    </FilterProvider>
+  );
 }
