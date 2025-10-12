17e75987ed3a2fbf2e580d136405b49d78f3cf17
cla6shade <cla6shade@github.com>
2025-10-12 19:41:06 +0900

    feat: teacher creation

---
+++ src/features/teachers/hooks/useCreateTeacher.ts
@@ -0,0 +1,31 @@
+import { useUpdate } from "@/hooks/useUpdate";
+import {
+  CreateTeacherRequestSchema,
+  CreateTeacherResponse,
+} from "@/app/(teachers)/api/teachers/schema";
+import z from "zod";
+
+export function useCreateTeacher() {
+  const { update, isSaving, error, clearError } = useUpdate<
+    z.input<typeof CreateTeacherRequestSchema>,
+    CreateTeacherResponse
+  >();
+
+  const createTeacher = async (
+    teacherData: z.input<typeof CreateTeacherRequestSchema>
+  ) => {
+    return await update(teacherData, {
+      endpoint: "/api/teachers",
+      method: "POST",
+      successMessage: "선생님이 성공적으로 생성되었습니다.",
+      errorMessage: "선생님 생성에 실패했습니다.",
+    });
+  };
+
+  return {
+    createTeacher,
+    isSaving,
+    error,
+    clearError,
+  };
+}
