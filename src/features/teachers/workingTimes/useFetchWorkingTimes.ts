213b4b81927491b0f97a6cf0e1de0313f36b7cdf
cla6shade <cla6shade@github.com>
2025-10-16 21:34:17 +0900

    feat: 시간표 조정 ui 구현

---
+++ src/features/teachers/workingTimes/useFetchWorkingTimes.ts
@@ -0,0 +1,15 @@
+import { useFetch } from "@/hooks/useFetch";
+import { GetWorkingTimesResponse } from "@/app/(table)/api/working-times/schema";
+
+export function useFetchWorkingTimes() {
+  const { data, loading, error, refetch } =
+    useFetch<GetWorkingTimesResponse>("/api/working-times");
+
+  return {
+    workingTimes: data?.data.times || [],
+    openHours: data?.data.openHours || { id: 0, startHour: 0, endHour: 23 },
+    loading,
+    error,
+    refetch,
+  };
+}
