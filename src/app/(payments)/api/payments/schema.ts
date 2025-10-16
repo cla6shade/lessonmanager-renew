fd40467dd998017cd75f2ad67458216ec6abf18c
cla6shade <cla6shade@github.com>
2025-10-16 20:49:02 +0900

    feat: payment creation 스키마 정의

--- src/app/(payments)/api/payments/schema.ts
+++ src/app/(payments)/api/payments/schema.ts
@@ -34,3 +34,27 @@ export type UpdatePaymentResponse = z.infer<typeof UpdatePaymentResponseSchema>;
 export type UpdatePaymentsResponse = z.infer<
   typeof UpdatePaymentsResponseSchema
 >;
+
+export const CreatePaymentRequestSchema = z.object({
+  userId: z.coerce.number().int().positive(),
+  months: z.coerce.number().int().min(1),
+  lessonCount: z.coerce.number().int(),
+  isReregister: z.coerce.boolean(),
+  method: z.coerce.number().int(),
+  startDate: z.iso
+    .datetime()
+    .transform((val) => (val ? toKstDate(val) : undefined))
+    .optional(),
+  endDate: z.iso
+    .datetime()
+    .transform((val) => (val ? toKstDate(val) : undefined))
+    .optional(),
+  memo: z.string().optional(),
+  paymentAmount: z.coerce.number().int().min(0),
+  isStartDateNonSet: z.coerce.boolean().optional(),
+});
+
+export const CreatePaymentResponseSchema = DataResponseSchema(PaymentSchema);
+
+export type CreatePaymentRequest = z.infer<typeof CreatePaymentRequestSchema>;
+export type CreatePaymentResponse = z.infer<typeof CreatePaymentResponseSchema>;
