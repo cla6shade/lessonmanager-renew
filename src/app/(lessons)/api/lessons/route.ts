c00180da757730d1647748b00c24824289baa6af
cla6shade <cla6shade@github.com>
2025-10-26 14:28:39 +0900

    refactor: 의미없는 else문 제거

--- src/app/(lessons)/api/lessons/route.ts
+++ src/app/(lessons)/api/lessons/route.ts
@@ -156,92 +156,91 @@ export async function POST(request: NextRequest) {
       return NextResponse.json<CreateLessonByAdminResponse>({
         data: lessonCreation,
       });
-    } else {
-      sessionUserId = sessionUserId!;
-      const sessionUser = await prisma.user.findUnique({
-        where: { id: sessionUserId },
-      });
-      if (!sessionUser) {
-        return NextResponse.json(
-          { error: "사용자를 찾을 수 없습니다" },
-          { status: 404 }
-        );
-      }
-      const input = CreateLessonByUserInputSchema.parse(await request.json());
-      const [
-        isLessonCountLeft,
-        isLessonInPayment,
-        isTimeBannedAt,
-        isLessonAvailableAt,
-        isTeacherWorkingHour,
-      ] = await Promise.all([
-        hasLessonCount(sessionUserId),
-        isLessonDueInPayment(input.dueDate, sessionUserId),
-        isBannedAt(input.dueDate, input.dueHour, input.teacherId),
-        isAvailableAt(
-          input.dueDate,
-          input.dueHour,
-          input.teacherId,
-          input.locationId,
-          input.isGrand
-        ),
-        isTeacherAvailableAt(input.teacherId, input.dueDate, input.dueHour),
-      ]);
-      if (!isLessonCountLeft) {
-        return NextResponse.json(
-          { error: "레슨 횟수가 부족합니다" },
-          { status: 400 }
-        );
-      }
-      if (!isLessonInPayment) {
-        return NextResponse.json(
-          {
-            error:
-              "결제 기간을 벗어난 날짜입니다. 다른 날짜로 다시 시도해주세요",
-          },
-          { status: 400 }
-        );
-      }
-      if (isTimeBannedAt) {
-        return NextResponse.json(
-          { error: "해당 시간은 예약이 제한되어 있습니다" },
-          { status: 400 }
-        );
-      }
-      if (!isLessonAvailableAt) {
-        return NextResponse.json(
-          { error: "다른 레슨과 예약 시간이 겹칩니다" },
-          { status: 400 }
-        );
-      }
-      if (!isTeacherWorkingHour) {
-        return NextResponse.json(
-          { error: "강사가 근무하지 않는 시간입니다" },
-          { status: 400 }
-        );
-      }
-
-      const tx: PrismaPromise<any>[] = [
-        createLessonByUser(
-          input,
-          sessionUserId,
-          sessionUser.name
-        ) as PrismaPromise<Lesson>,
-        useLessonCount(sessionUserId, 1) as PrismaPromise<User>,
-      ];
-      const [lessonCreation, lessonCountUpdate] = await prisma.$transaction(tx);
-      await createModifyHistory({
-        isAdmin,
-        userId: sessionUserId,
-        teacherId: input.teacherId,
-        lesson: lessonCreation,
-        createdById: sessionUserId,
-        type: LessonModifyType.CREATE_LESSON,
-      });
-      return NextResponse.json<CreateLessonByUserResponse>({
-        data: lessonCreation,
-      });
     }
+
+    sessionUserId = sessionUserId!;
+    const sessionUser = await prisma.user.findUnique({
+      where: { id: sessionUserId },
+    });
+    if (!sessionUser) {
+      return NextResponse.json(
+        { error: "사용자를 찾을 수 없습니다" },
+        { status: 404 }
+      );
+    }
+    const input = CreateLessonByUserInputSchema.parse(await request.json());
+    const [
+      isLessonCountLeft,
+      isLessonInPayment,
+      isTimeBannedAt,
+      isLessonAvailableAt,
+      isTeacherWorkingHour,
+    ] = await Promise.all([
+      hasLessonCount(sessionUserId),
+      isLessonDueInPayment(input.dueDate, sessionUserId),
+      isBannedAt(input.dueDate, input.dueHour, input.teacherId),
+      isAvailableAt(
+        input.dueDate,
+        input.dueHour,
+        input.teacherId,
+        input.locationId,
+        input.isGrand
+      ),
+      isTeacherAvailableAt(input.teacherId, input.dueDate, input.dueHour),
+    ]);
+    if (!isLessonCountLeft) {
+      return NextResponse.json(
+        { error: "레슨 횟수가 부족합니다" },
+        { status: 400 }
+      );
+    }
+    if (!isLessonInPayment) {
+      return NextResponse.json(
+        {
+          error: "결제 기간을 벗어난 날짜입니다. 다른 날짜로 다시 시도해주세요",
+        },
+        { status: 400 }
+      );
+    }
+    if (isTimeBannedAt) {
+      return NextResponse.json(
+        { error: "해당 시간은 예약이 제한되어 있습니다" },
+        { status: 400 }
+      );
+    }
+    if (!isLessonAvailableAt) {
+      return NextResponse.json(
+        { error: "다른 레슨과 예약 시간이 겹칩니다" },
+        { status: 400 }
+      );
+    }
+    if (!isTeacherWorkingHour) {
+      return NextResponse.json(
+        { error: "강사가 근무하지 않는 시간입니다" },
+        { status: 400 }
+      );
+    }
+
+    const tx: PrismaPromise<any>[] = [
+      createLessonByUser(
+        input,
+        sessionUserId,
+        sessionUser.name
+      ) as PrismaPromise<Lesson>,
+      useLessonCount(sessionUserId, 1) as PrismaPromise<User>,
+    ];
+    const [lessonCreation, lessonCountUpdate] = await prisma.$transaction(tx);
+    await createModifyHistory({
+      isAdmin,
+      userId: sessionUserId,
+      teacherId: input.teacherId,
+      lesson: lessonCreation,
+      createdById: sessionUserId,
+      type: LessonModifyType.CREATE_LESSON,
+    });
+    return NextResponse.json<CreateLessonByUserResponse>({
+      data: lessonCreation,
+    });
   } catch (error) {
     console.error(error);
     return buildErrorResponse(error);
