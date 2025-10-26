5467cc7a52dc9f3d907e4401c6622014b880e7f3
cla6shade <cla6shade@github.com>
2025-10-26 14:35:39 +0900

    feat: 메시지 전송 흐름 변경

--- src/app/(sms)/api/sms/route.ts
+++ src/app/(sms)/api/sms/route.ts
@@ -1,7 +1,8 @@
 import { buildErrorResponse } from "@/app/utils";
 import { getSession } from "@/lib/session";
 import { NextRequest, NextResponse } from "next/server";
-import { SendSMSRequestSchema } from "./schema";
+import { GetSMSTargetRequestSchema, SendSMSRequestSchema } from "./schema";
+// import { sendMessage } from "./service";
 
 export async function POST(request: NextRequest) {
   try {
@@ -9,9 +10,36 @@ export async function POST(request: NextRequest) {
     if (!isAdmin) {
       return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
     }
-    const { receiverType, message, targets, selectAll } =
-      SendSMSRequestSchema.parse(request.body);
+    const {
+      receiverType,
+      message,
+      targets,
+      isTotalSelected,
+      selectedLocationId,
+    } = SendSMSRequestSchema.parse(request.body);
+    // const sendResult = await sendMessage({
+    //   receiverType,
+    //   message,
+    //   targets,
+    //   isTotalSelected,
+    //   selectedLocationId,
+    // });
+    // console.log(sendResult);
   } catch (error) {
+    return buildErrorResponse(error);
+  }
+}
+
+export async function GET(request: NextRequest) {
+  try {
+    const { isAdmin } = await getSession();
+    if (!isAdmin) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+    const { receiverType, selectedLocationId } =
+      GetSMSTargetRequestSchema.parse(request.body);
+  } catch (error) {
+    console.error(error);
     return buildErrorResponse(error);
   }
 }
