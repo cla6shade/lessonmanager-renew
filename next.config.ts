779d398340288908f21f9037b13e733168801446
cla6shade <cla6shade@github.com>
2025-10-27 16:33:52 +0900

    chore: next 16 업데이트 이후 차크라 관련 설정 적용

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   compiler: {
     emotion: true,
   },
+  experimental: {
+    optimizePackageImports: ["@chakra-ui/react"],
+  },
 };
 
 export default nextConfig;
